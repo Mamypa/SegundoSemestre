af96ee35082084454c44834656a53fc8370e6f02
Mamypa <victorbomfim@live.com>
2025-04-22 11:07:16 -0300

    Add files via upload

---
+++ react/src/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Home.css";
+
+const Home = () => {
+
+    return (
+        <div>
+            <nav>
+                <h1>Depositório de Atividades</h1>
+                <ul className='link-list'>
+                    <li>
+                        <Link to='/Atv01'>Atividade 1</Link>
+                    </li>
+                    <li>
+                        <Link to='/Atv02'>Atividade 2</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+export default Home;
