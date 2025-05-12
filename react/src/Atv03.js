9369417c09f0f8295de630d849cdeabdded88a98
Mamypa <victorbomfim@live.com>
2025-05-12 10:56:35 -0300

    feat(Gallery): add Gallery component with Profile display

---
+++ react/src/Atv03.js
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+import Gallery from "./Gallery";
+
+
+export default function Atv03() {
+    return ( 
+        <>
+            <h1>Atividade 03</h1>
+            <div className="container">
+                <Gallery />
+            </div>
+            <Link to='/' className="botao-voltar">Voltar</Link>
+        </>
+    )
+
+
+}
