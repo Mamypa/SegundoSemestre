b1efb1f56c7b7716d93af8d4d8b877dfcf02f520
Mamypa <victorbomfim@live.com>
2025-05-12 11:42:43 -0300

    feat(TodoList): enhance avatar styling with inline styles

--- react/src/TodoList.js
+++ react/src/TodoList.js
@@ -2,14 +2,20 @@ const person = {
     name: "Mouse",
     theme :{
         backgroundColor: "black",
-        color: "pink"
+        color: "pink",
+        avatar: {
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%"
+        }
     }
 }
+
 export default function TodoList() {
     return (
         <div style={person.theme}>
             <h1>Lista de Tarefas do {person.name}</h1>
-            <img className="avatar" src="https://i.imgur.com/NhSLQP7.png" alt="Mouse" />
+            <img style={person.theme.avatar} src="https://i.imgur.com/NhSLQP7.png" alt="Mouse" />
             <ul>
                 <li>DORMIR</li>
                 <li>DORMIR</li>
