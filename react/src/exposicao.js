bf161d9b0b339028885b0871532fc9ff3f11838e
Mamypa <victorbomfim@live.com>
2025-05-27 11:31:04 -0300

    exposicao

--- react/src/exposicao.js
+++ react/src/exposicao.js
@@ -3,7 +3,7 @@ import { sculptureList } from "./data.js";
 
 export default function Exposicao() {
     const [index, setIndex] = useState(0);
-    const [showMore, setShowMore] = useState(0);
+    const [showMore, setShowMore] = useState(false);
 
     function handleNextClick() {
         setIndex(index + 1);
@@ -12,27 +12,29 @@ export default function Exposicao() {
     function handleMoreClick() {
         setShowMore(!showMore);
     }
-    let sculputure = sculptureList[index];
+    let sculpture = sculptureList[index];
     return (
         <>
             <button onClick={handleNextClick}>
                 Proximo
             </button>
             <h2>
-                <i>{sculputure.name}</i>
-                marca:{sculputure.marca}
-                fabricante:{sculptureList.fabricante}
+                <i>{sculpture.name}</i>
+                marca:{sculpture.marca}
+                fabricante:{sculpture.fabricante}
             </h2>
             <h3>
-                ({index + 1} of {sculptureList.lenght})
+                ({index + 1} of {sculptureList.length})
             </h3>
             <button onClick={handleMoreClick}>
                 {showMore ? 'Hide' : 'Show'} Details
             </button>
-            {showMore && <p>{sculptureList.descricao}</p>}
+            {showMore && <p>{sculpture.descricao}</p>}
             <img
-                src={sculputure.url}
-                alt={sculptureList.alt}
+                src={sculpture.url}
+                alt={sculpture.alt}
+                width={500}
+                height={500}
             />
         </>
     );
